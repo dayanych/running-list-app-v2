1aba283ab30ebfd827d89f73ea1261cf167c7dd9
dayanych <dayana.shakenova20@gmail.com>
2024-05-28 22:58:34 +0500

    feat: add mui

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import 'src/assets/styles/index.css';
 
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'src/app';
+import { responsiveFontSizesTheme } from 'src/assets/theme/theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={responsiveFontSizesTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
